6cae6bae4876515b1a00a6f1ffb7be4f1c3f06f5
Ernest <ernestduodu@hotmail.co.uk>
2021-09-15 10:15:20 +0100

    Implemented programatic (Imperative)  navigaation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 import QuoteForm from '../components/quotes/QuoteForm'
 const NewQuote = () => {
-
+    const history = useHistory();
+    
     const addQuoteHandler = (quote) => {
-        console.log(quote)
+        console.log(quote);
+
+        history.push('/quotes') //Note this will allow the user to use the back button to visit the previous page
+        //history.replace('/quotes') //Note this will replace the current page hence user's can't navigate back to the previous page 
     }
     return (
         <QuoteForm onAddQuote={addQuoteHandler}/>
