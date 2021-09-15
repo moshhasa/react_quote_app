edf5f58cd3fa798c3c9681f45fe4bbdf57a2d17c
Ernest <ernestduodu@hotmail.co.uk>
2021-09-15 10:09:11 +0100

    Added a 404 (not found) route and page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = props => {
+    return (
+        <div className='centered'>
+            <p>Page not Found</p>
+        </div>
+    )
+}
+
+
+export default NotFound
