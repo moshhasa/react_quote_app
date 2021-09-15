9f2fe6eb8a2c7866d0f459cd444a527e11ec43ea
Ernest <ernestduodu@hotmail.co.uk>
2021-09-15 10:25:02 +0100

    Preventing Possibly Unwanted Route Transitions with the

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { Prompt } from 'react-router';
 import Card from '../UI/Card'
 
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,6 +8,7 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [formHasFocus, setFormHasFocus] = useState(false)
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,9 +21,19 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const formFocusHandler = () => {
+    setFormHasFocus(true)
+  }
+
+  const finishedEditingHandler = () => {
+    setFormHasFocus(false);
+  }
+
   return (
+    <>
+    <Prompt when={formHasFocus} message={() => "Are you sure you want to leave this page?"}/>
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form onFocus={formFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +49,11 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={finishedEditingHandler} className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </>
   );
 };
 
