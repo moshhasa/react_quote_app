6cd69f5a40b5e86847f34b8b24f38155b46699d1
Ernest <ernestduodu@hotmail.co.uk>
2021-09-15 10:56:04 +0100

    Use React Route to conditionally show some content on a page without the use of state

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, useParams } from 'react-router'
+import { Link } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -15,6 +16,11 @@ const QuoteDetails = () => {
     return (
         <>
           {quote ?  <HighlightedQuote text={quote.text} author={quote.author}/>  : <p>No quote</p>}
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Show Comments</Link>
+                </div>
+          </Route>
           <Route  path={`/quotes/${params.quoteId}/comments`}>
               <Comments />
           </Route>
