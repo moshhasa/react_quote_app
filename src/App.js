34c01997eb3d2eec700e1ba80d9c6e130761ccaf
Ernest <ernestduodu@hotmail.co.uk>
2021-09-15 10:08:50 +0100

    Added a 404 (not found) route and page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Redirect, Route, Switch } from "react-router";
 import Layout from "./components/layout/Layout";
 import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 import QuoteDetails from "./pages/QuoteDetails";
 import Quotes from "./pages/Quotes";
 
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route path="/new-quote" >
             <NewQuote />
+          </Route>
+          <Route path='*'>
+            <NotFound/>
           </Route>
         </Switch>
     </Layout>
