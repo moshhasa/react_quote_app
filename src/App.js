9dfd38727eecc6ef87f657396cc94227282dc9bc
Ernest <ernestduodu@hotmail.co.uk>
2021-09-16 09:25:44 +0100

    Implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import Layout from "./components/layout/Layout";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetails from "./pages/QuoteDetails";
-import Quotes from "./pages/Quotes";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetails = React.lazy(() => import("./pages/QuoteDetails"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const Quotes = React.lazy(() => import("./pages/Quotes"));
 
 function App() {
   return (
     <Layout>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
         <Switch>
           <Route path="/" exact>
             <Redirect to="quotes" />
@@ -15,16 +26,17 @@ function App() {
           <Route path="/quotes" exact>
             <Quotes />
           </Route>
-          <Route path="/quotes/:quoteId" >
+          <Route path="/quotes/:quoteId">
             <QuoteDetails />
           </Route>
-          <Route path="/new-quote" >
+          <Route path="/new-quote">
             <NewQuote />
           </Route>
-          <Route path='*'>
-            <NotFound/>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
+      </Suspense>
     </Layout>
   );
 }
